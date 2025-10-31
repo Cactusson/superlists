b5a4bca418e26d8d4c09bd64b967722c362e663f
cactusson <matt8th@gmail.com>
2025-10-31 12:01:40 +0300

    switch to nonroot user

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -25,10 +25,12 @@ if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
     ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+    db_path = os.environ["DJANGO_DB_PATH"]
 else:
     DEBUG = True
     SECRET_KEY = "django-insecure-2tlg_j4+x&(%zebz*l1&yh@4%pbry!#uq((u64k(4_m614gqiu"
     ALLOWED_HOSTS = []
+    db_path = BASE_DIR / "db.sqlite3"
 
 
 # Application definition
@@ -80,7 +82,7 @@ WSGI_APPLICATION = "superlists.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": db_path,
     }
 }
 
