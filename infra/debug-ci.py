0d8bb0ea303904f91e15967bd7128668392009ed
cactusson <matt8th@gmail.com>
2025-11-21 08:05:51 +0300

    fix 14

--- infra/debug-ci.py
+++ infra/debug-ci.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
-# webdriver.Firefox().quit()
-options = webdriver.FirefoxOptions()
-service = webdriver.FirefoxService(executable_path="/usr/local/bin/geckodriver")
-webdriver.Firefox(options=options, service=service).quit()
+webdriver.Firefox().quit()
+# options = webdriver.FirefoxOptions()
+# service = webdriver.FirefoxService(executable_path="/usr/local/bin/geckodriver")
+# webdriver.Firefox(options=options, service=service).quit()
 print("All good")
